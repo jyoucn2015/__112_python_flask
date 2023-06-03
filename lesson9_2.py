fc57857adb5ba3b1bbeb7302019fe0acd5f52eb6
jyoucn2015 <jyoucn2015@gmail.com>
2023-06-03 11:27:43 +0800

    st.dataframe

--- lesson9_2.py
+++ lesson9_2.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
 
 scores_array = np.random.randint(50,high=101,size=(50,5))
+#print(scores_array)
 student_dataFrame = pd.DataFrame(data=scores_array,
             columns=["國文","英文","數學","地理","社會"],
             index=range(1,51))
-print(student_dataFrame)
+#print(student_dataFrame)
+
+st.header("3年5班成績表")
+#st.table(data=student_dataFrame)
+st.dataframe(data=student_dataFrame)
